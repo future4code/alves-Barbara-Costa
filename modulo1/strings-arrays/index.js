f8a5370ed2ea49be5ca261232bf01ccf7bb89e1e
barbara-carrilho <carrilhobarbara97@gmail.com>
2022-05-11 21:34:19 -0300

    exercicio de arrays

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,49 @@
+//Exercícios de interpretação de código
+
+//(1)
+//  let array
+// console.log('a. ', array)
+// //R= este comando declara uma variavél com nome array
+
+// array = null
+// console.log('b. ', array)
+// //R= apresenta um valor nulo
+
+// array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// console.log('c. ', array.length)
+// //R= mostra o tamanho de uma array
+
+// let i = 0
+// console.log('d. ', array[i])
+// // R= o comando let declara uma variavel, sendo assim ele está dando um valor ao "i" 
+// //que geralmente é visto como "indice" ou "indefinido"
+
+// array[i+1] = 19
+// console.log('e. ', array)
+// //R= atribui valor ao indice do array
+
+// const valor = array[i+6]
+// console.log('f. ', valor)
+// //R= fixa valor da way e imprime a "casa" que ela está que no caso é a 9
+
+//(2)
+// const frase = prompt("Digite uma frase")
+// console.log(frase.toUpperCase().replaceAll("A", "I"), frase.length)
+// //R= os comando utilizados fizeram com que a frase do usuario ficasse maiuscula e atraves do comando "replaceAll" fez com que fosse trocado o "A" pelo "I"
+// // a frase ficou "SUBI NUM ÔNIBUS EM MIRROCOS 27"
+
+// //...............................................................................................................// 
+
+// // Exercícios de escrita de código
+// // (1)
+// // const nomeUsuario = prompt("Digite seu nome ")
+// // const emailDoUsuario = prompt ("digite seu email")
+// // const fraseTemplateString = `Seja bem-vindes! seu email foi cadastrado com sucesso! ${nomeUsuario}`
+// // console.log(fraseTemplateString)
+
+// // (2)
+
+// let arrayComidasFavoritas = ["Pizza" , "sushi" , "Churrasco" , "cachorro-quente" , "banana-frita"] 
+// const fraseTemplateString = `essas são minhas comidas favoritas`
+
+// console.log(`${fraseComidaPreferida}\n${comidasPreferidas[0]}\n${comidasPreferidas[1]}\n${comidasPreferidas[2]}\n${comidasPreferidas[3]}\n${comidasPreferidas[4]}`)
