dcc530e6e8ae494c6956238a6042f2a5fa3addb6
barbara-carrilho <carrilhobarbara97@gmail.com>
2022-05-19 22:35:05 -0300

    exericio funcoes de array adicionado

---
+++ modulo1/funcoes-de-array/index.js
@@ -0,0 +1,44 @@
+// EXERCICIO DE INTERPRETAÇÃO DE CODIGO
+
+//(1)
+// R= aparecerá o nome do usuario e apelido, utilizando o comando map, este comando pode pecorrer 
+//por todo o array. dentro dos() tem a função item que busca o objeto e o comando index tem a função de buscar o
+// indice de um array
+// const usuarios = [
+//     { nome: "Amanda Rangel", apelido: "Mandi" },
+//     { nome: "Laís Petra", apelido: "Laura" },
+//     { nome: "Letícia Chijo", apelido: "Chijo" }
+//   ]
+  
+//   const novoArrayA = usuarios.map((item, index, array) => {
+//      console.log(item, index, array)
+//   })
+
+// (2) 
+//  a) O que vai ser impresso no console?
+//  R= será impresso a segunda linha do array, através do comando map chamando o 
+//  index, pedindo return do nome e apelido de leticia chijo
+
+// const usuarios = [
+//     { nome: "Amanda Rangel", apelido: "Mandi" },
+//     { nome: "Laís Petra", apelido: "Laura" },
+//     { nome: "Letícia Chijo", apelido: "Chijo" },
+//   ]
+  
+//   const novoArrayB = usuarios.map((item, index, array) => {
+//      return item.nome
+//   })
+  
+//   console.log(novoArrayB)
+
+
+//EXERCICIO DE ESCRITA DE CODIGO
+
+// a) Crie um novo array que contenha apenas o nome dos doguinhos
+
+// b) Crie um novo array apenas com os [cachorros salsicha](https://www.youtube.com/watch?v=YQ404vwjzus)
+
+// c) Crie um novo array que possuirá mensagens para enviar para todos os clientes que são poodles. 
+// A mensagem deve ser: "Você ganhou um cupom de desconto de 10% para tosar o/a `[NOME]`!"
+
+
